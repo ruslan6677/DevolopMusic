03c55630da4e5ba2cb0fa8e91a5d1d4f769770ff
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-10-16 09:28:03 +0400

    Add files via upload

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID", 6))
-    API_HASH = os.environ.get("API_HASH", None)
-    BOT_OWNER = os.environ.get("BOT_OWNER", "")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "MusicAzBot")
-    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", None)
-    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", None))
+    API_ID = int(os.environ.get("API_ID"))
+    API_HASH = os.environ.get("API_HASH")
+    BOT_OWNER = os.environ.get("BOT_OWNER")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME")
+    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID"))
