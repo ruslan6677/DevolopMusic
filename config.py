2e68e879596220ccd14473d66d170a4909528457
RuslanBTB <87086724+ruslan6677@users.noreply.github.com>
2022-12-19 10:58:50 +0400

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     BOT_TOKEN = os.environ.get("5802781802:AAHTqszeFIeFw3DZgGAQn34HVdrELadC0TI")
-    API_ID = int(os.environ.get("API_ID", "10300036"))
+    API_ID = int(os.environ.get("API_ID", ""))
     API_HASH = os.environ.get("79c295e05c970ddd724f0762ba275104")
     BOT_OWNER = os.environ.get("ordayam_5_deqiqeye")
     BOT_USERNAME = os.environ.get("DevolopMusic_bot")
