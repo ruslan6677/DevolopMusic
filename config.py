e0367d75447d31f4862604fe1992ec0d1ef79b7c
RuslanBTB <87086724+ruslan6677@users.noreply.github.com>
2023-01-17 00:22:30 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_OWNER = os.environ.get("BOT_OWNER")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5989349944:AAHWwg-cF--N508MAXgfU2tt84AL2c52fXg")
+    API_ID = int(os.environ.get("API_ID", 10300036))
+    API_HASH = os.environ.get("API_HASH", "79c295e05c970ddd724f0762ba275104")
+    BOT_OWNER = os.environ.get("BOT_OWNER", "ordayam_5_deqiqeye")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "DevolopingMusicbot")
+    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "DevolopNuray")
     CHANNEL = os.environ.get("CHANNEL")
-    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID"))
+    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001896885722"))
     ETIRAF_BOT = os.environ.get("ETIRAF_BOT")
     ETIRAF_KANALI = os.environ.get("ETIRAF_KANALI")
