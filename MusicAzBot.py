2121d3a454ef20b8820e3af24e46d90cf8c57d79
RuslanBTB <87086724+ruslan6677@users.noreply.github.com>
2023-01-17 00:43:00 +0400

    Update MusicAzBot.py

--- MusicAzBot.py
+++ MusicAzBot.py
@@ -6,13 +6,40 @@ from config import Config
 from youtube_search import YoutubeSearch
 from pyrogram.handlers import MessageHandler
 from yt_dlp import YoutubeDL
-from pyrogram import Client, filters
+from io import BytesIO
+from aiohttp import ClientSession
+from pyrogram import Client, filters, idle
 import yt_dlp
 from pyrogram.types import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
     Message
 )
+
+
+aiohttpsession = ClientSession()
+
+async def get_http_status_code(url: str) -> int:
+    async with aiohttpsession.head(url) as resp:
+        return resp.status
+
+
+async def make_carbon(code):
+    url = "https://carbonara.vercel.rphn/api/cook"
+    async with aiohttpsession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
+
+
+@bot.on_message(filters.command("carbon"))
+async def carbon_func(bot: rphn, msg: Message):
+    m = await msg.reply_text("`Hazırlanır`")
+    carbon = await make_carbon(msg.reply_to_message.text)
+    await m.edit("`Göndərilir`")
+    await bot.send_photo(msg.chat.id, photo=carbon)
+    await m.delete()
+    carbon.close()
 
 
 #config#
@@ -169,5 +196,7 @@ async def vsong(client, message):
         print(e)
 
 
-
+bot.start()
+print("Bot başladı!")
+idle()
 bot.run()
